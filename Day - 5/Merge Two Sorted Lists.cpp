be640f2d73e7ccda5cb9fd3b3bb7b6f683815be2
HariHaran <haran465@gmail.com>
2021-04-03 15:55:18 +0530

    Update Merge Two Sorted Lists.cpp

--- Day - 5/Merge Two Sorted Lists.cpp
+++ Day - 5/Merge Two Sorted Lists.cpp
@@ -34,3 +34,35 @@ public:
         return dummy->next;
     }
 };
+
+
+// Hari's
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *ans = new ListNode(0);
+        ListNode *res = ans;
+        while(l1 != nullptr && l2 != nullptr){
+            if(l1->val < l2->val){
+                res->next = l1;
+                l1 = l1->next;
+            }
+            else if(l1->val >= l2->val){
+                res->next = l2;
+                l2 = l2->next;
+            }
+            res = res->next;
+        }
+        
+        if(l1 != nullptr){
+            res->next = l1;
+        }
+        
+        if(l2 != nullptr){
+            res->next = l2;
+        }
+               
+        return ans->next;
+    }
+};
