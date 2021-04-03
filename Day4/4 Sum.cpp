86e378d065e7b480ec40b62861ef37aeb6382978
HariHaran <haran465@gmail.com>
2021-04-03 10:43:21 +0530

    Update 4 Sum.cpp

--- Day4/4 Sum.cpp
+++ Day4/4 Sum.cpp
@@ -40,3 +40,42 @@ public:
         return res;
     }
 };
+
+
+// Hari's
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        set<vector<int>> s;
+        int N = nums.size();
+        
+        for(int i = 0; i<N-1; i++){
+            vector<int> temp;
+            for(int j = i+1; j<N; j++){
+                int l = j+1, r = N-1;
+                int find = target - nums[i] - nums[j];
+                while(l < r){
+                    if(nums[l]+nums[r] == find){
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[j]);
+                        temp.push_back(nums[l]);
+                        temp.push_back(nums[r]);
+                        s.insert(temp);
+                        l++; r--;
+                        temp.clear();
+                    }
+                    else if(nums[l]+nums[r] < find) l++;
+                    else r--;
+                }
+            }
+        }
+        
+        for(auto it: s){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
