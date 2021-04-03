f69dcfca425e023d05d86f8fa4cae557e0d331c3
HariHaran <haran465@gmail.com>
2021-04-03 15:40:57 +0530

    Update Middle Of Linked List.cpp

--- Day - 5/Middle Of Linked List.cpp
+++ Day - 5/Middle Of Linked List.cpp
@@ -24,3 +24,19 @@ public:
         return prev;
     }   
 };
+
+// Hari's
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
