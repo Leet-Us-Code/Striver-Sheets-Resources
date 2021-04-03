7a2930883cdc74482f70ade1936c9083fcf46081
HariHaran <haran465@gmail.com>
2021-04-03 10:23:33 +0530

    Update 2 Sum.cpp

--- Day4/2 Sum.cpp
+++ Day4/2 Sum.cpp
@@ -28,3 +28,29 @@ public:
         return res;
     }
 };
+
+// Hari's
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> vec;
+        vector<int> res;
+        for(int i = 0; i<nums.size(); i++){
+            vec.push_back(make_pair(nums[i], i));
+        }
+        sort(vec.begin(), vec.end());
+        int p1 = 0, p2 = vec.size()-1;
+        while(p1 < p2){
+            if(vec[p1].first + vec[p2].first == target){
+                res.push_back(vec[p1].second);
+                res.push_back(vec[p2].second);
+                return res;
+            }
+            else if(vec[p1].first + vec[p2].first < target){
+                p1++;
+            }
+            else p2--;
+        }
+        return res;
+    }
+};
