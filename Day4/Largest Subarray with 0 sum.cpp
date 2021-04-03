632e4e778bde4b258e1c15ff8772a6f1f0f8eb7d
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-03 15:19:22 +0530

    Update Largest Subarray with 0 sum.cpp

--- Day4/Largest Subarray with 0 sum.cpp
+++ Day4/Largest Subarray with 0 sum.cpp
@@ -19,3 +19,29 @@ int maxLen(int A[], int n)
     }
     return maxLen;
 }
+
+
+//Sid's solution
+int maxLen(int a[], int n)
+{
+    int sum[n+1];
+    sum[0] = 0;
+    for(int i = 1; i < n+1; i++)
+    {
+        sum[i] = sum[i-1] + a[i-1];
+    }
+    map<int, int> m;
+    int length = 0;
+    for(int i = 0; i < n+1; i++)
+    {
+        if(m[sum[i]] == 0)
+        {
+            m[sum[i]] = i+1;
+        }
+        else 
+        {
+            length = max(length, i + 1 - m[sum[i]]);
+        }
+    }
+    return length;
+}
