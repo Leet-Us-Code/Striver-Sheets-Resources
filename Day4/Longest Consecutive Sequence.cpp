3a6300fa0833f6278bf041df44e4573e76cfbbf9
HariHaran <haran465@gmail.com>
2021-04-03 14:49:28 +0530

    Update Longest Consecutive Sequence.cpp

--- Day4/Longest Consecutive Sequence.cpp
+++ Day4/Longest Consecutive Sequence.cpp
@@ -32,3 +32,29 @@ public:
         return maxL;
     }
 };
+
+// Hari's 
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> hashSet;
+        for(auto it: nums) hashSet.insert(it);
+        
+        int maxStreak = 0;
+        for(int it: nums){
+            if(!hashSet.count(it-1)){
+                // if "it" is the smallest no. in a seq, start count from that "it"
+                int currNum = it, currStreak = 1;
+                
+                while(hashSet.count(currNum+1)){
+                    currNum = currNum+1;
+                    currStreak += 1;
+                    maxStreak = max(maxStreak, currStreak);
+                }
+                maxStreak = max(maxStreak, currStreak);
+            }
+        }
+        return maxStreak;
+    }
+};
