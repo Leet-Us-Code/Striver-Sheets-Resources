6f7f96a50294a507c94b7925383ff93c84eec1db
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-04 09:19:15 +0530

    Update Palindrome Linked List.cpp

--- Day - 6/Palindrome Linked List.cpp
+++ Day - 6/Palindrome Linked List.cpp
@@ -19,3 +19,42 @@ public:
         return true;
     }
 };
+
+//Siddharth's solution 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(ListNode **left, ListNode *right)
+    {
+        if(*left == NULL || right == NULL)
+            return true;
+        bool remaining = helper(&(*left), right->next);
+        if(remaining)
+        {
+            if((*left)->val == right->val)
+            {
+                (*left) = (*left)->next;
+                return true;
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode *left = head;
+        ListNode *right = head;
+        return helper(&left, right);
+    }
+};
