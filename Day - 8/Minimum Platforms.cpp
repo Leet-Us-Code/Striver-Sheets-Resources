15e46de1baed68aadc68f08c0dae2ce2d5f1c5c1
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-15 23:10:52 +0530

    Create Minimum Platforms.cpp

---
+++ Day - 8/Minimum Platforms.cpp
@@ -0,0 +1,22 @@
+int findPlatform(int arr[], int dep[], int n)
+    {
+        int i = 0, j = 0, plat = 0;
+        sort(arr,arr+n);
+        sort(dep,dep+n);
+        int maxP = INT_MIN;
+        while(i < n && j < n)
+        {
+            if(arr[i] <= dep[j])
+            {
+                plat++;
+                i++;
+            }
+            else 
+            {
+                plat--;
+                j++;
+            }
+            maxP = max(maxP, plat);
+        }
+        return maxP;
+    }
