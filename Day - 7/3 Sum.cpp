f90b22d50d79d11ab455ae1accba18a5257378a4
HariHaran <haran465@gmail.com>
2021-04-15 21:24:26 +0530

    Update 3 Sum.cpp

--- Day - 7/3 Sum.cpp
+++ Day - 7/3 Sum.cpp
@@ -44,3 +44,45 @@ public:
         return output;
     }
 };
+
+
+// Hari's
+// Do not use sets as insertions is O(logN) and we have to move it to 2-D vec also
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+        //set<vector<int>> s;
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        int l, r;
+        
+        for(int i = 0; i<nums.size(); i++){
+            l = i+1;
+            r = nums.size()-1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0) r--;
+                else if(sum < 0) l++;
+                else {
+                    vector<int> ans;
+                    ans.push_back(nums[i]);
+                    ans.push_back(nums[l]);
+                    ans.push_back(nums[r]);
+                    //s.insert(ans);
+                    res.push_back(ans);
+                    ans.clear();
+                    //removing duplicates here also 
+                    while(r > l && nums[l] == nums[l+1])
+                        l++;
+                    while(r > l && nums[r] == nums[r-1])
+                        r--;
+                    
+                    l++; r--;
+                }
+            }
+            while(i < nums.size()-1 && nums[i] == nums[i+1]) i++;
+        }
+        // for(auto it: s){
+        //     res.push_back(it);
+        // }
+        return res;
+    }
