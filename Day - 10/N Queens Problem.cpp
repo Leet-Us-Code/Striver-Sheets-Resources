ddff83e101b0786bb3988b9840ae04266ceb034e
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-25 22:51:11 +0530

    Update N Queens Problem.cpp

--- Day - 10/N Queens Problem.cpp
+++ Day - 10/N Queens Problem.cpp
@@ -55,3 +55,63 @@ public:
     
     
 };
+
+// Sid's naive solution
+
+class Solution {
+public:
+    bool isSafe(vector<string> board, int row, int col)
+    {
+        //i have to check the current row 
+        for(int i = 0; i < col; i++)
+        {
+            if(board[row][i] == 'Q')
+                return false;
+        }
+        for(int i = row, j = col; i < board.size() && j >= 0; i++, j--)
+        {
+            if(board[i][j] == 'Q')
+                return false;
+        }
+        for(int i = row, j = col; i >= 0 && j >= 0; i--,j--)
+        {
+            if(board[i][j] == 'Q')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(vector<vector<string>> &sol, vector<string> board, int col)
+    {
+        //base case 
+        if(col >= board.size())
+        {
+            sol.push_back(board);
+        }
+        for(int row = 0; row < board.size(); row++)
+        {
+            if(isSafe(board, row, col))
+            {
+                board[row][col] = 'Q';
+                solve(sol, board, col+1);
+                board[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board;
+        for(int i = 0; i < n; i++)
+        {
+            string s;
+            for(int j = 0; j < n; j++)
+            {
+                s.push_back('.');
+            }
+            board.push_back(s);
+        }
+        vector<vector<string>> sol;
+        solve(sol, board, 0);
+        return sol;
+    }
+};
