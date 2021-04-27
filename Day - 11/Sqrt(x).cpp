e0cb50908af5985f94f3428cba63d8f53fca1649
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-27 22:11:17 +0530

    Create Sqrt(x).cpp

---
+++ Day - 11/Sqrt(x).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0)
+            return 0;
+        else if(x == 1)
+            return 1;
+        int l = 0 , r = x;
+        int res = -1;
+        while(l <= r)
+        {
+            int mid = (int)((l+r)/2);
+            if(mid == x/mid)
+            {
+                return mid;
+            }
+            else if(mid < x/mid)
+            {
+                res = mid;
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+};
