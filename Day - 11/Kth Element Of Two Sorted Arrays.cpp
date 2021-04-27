0a4572a69df70847d37113ad510d60fe49bdd6f0
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-27 22:10:07 +0530

    Create Kth Element Of Two Sorted Arrays.cpp

---
+++ Day - 11/Kth Element Of Two Sorted Arrays.cpp
@@ -0,0 +1,47 @@
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        //OM GAN GANAPATHAYE NAMO NAMAH 
+        //JAI SHRI RAM 
+        //JAI BAJRANGBALI 
+        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+        int p1 = 0, p2 = 0;
+        int cntr = 0;
+        while(p1 < n && p2 < m)
+        {
+            if(arr1[p1] < arr2[p2])
+            {
+                cntr++;
+                if(cntr == k)
+                {
+                    return arr1[p1];
+                }
+                p1++;
+            }
+            else 
+            {
+                cntr++;
+                if(cntr == k)
+                {
+                    return arr2[p2];
+                }
+                p2++;
+            }
+        }
+        while(p1 < n)
+        {
+            cntr++;
+            if(cntr == k)
+            return arr1[p1];
+            p1++;
+        }
+        while(p2 < m)
+        {
+            cntr++;
+            if(cntr == k)
+            return arr2[p2];
+            p2++;
+        }
+    }
+};
