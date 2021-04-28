d78aac1efbee8e5300ea340e8945e41dd5ef98e9
HariHaran <haran465@gmail.com>
2021-04-28 11:21:13 +0530

    Added comments

--- Day - 13/BFS.cpp
+++ Day - 13/BFS.cpp
@@ -8,18 +8,18 @@ public:
         
         queue<TreeNode*> q;
         q.push(root);
-        q.push(NULL);
+        q.push(NULL); // a NULL denotes the end of a particular level. Next level of BFS starts after a NULL
         
         vector<int> ans;
         while(!q.empty()){
             TreeNode* curr = q.front(); q.pop();
-            if(curr == NULL){
-                res.emplace_back(ans);
-                ans.resize(0);
-                if(q.size() > 0) q.push(NULL);
+            if(curr == NULL){ // current level over
+                res.emplace_back(ans); //push particular lvl vector to ans
+                ans.resize(0); // same as ans.clear()
+                if(q.size() > 0) q.push(NULL); // if next lvl elements are there add a NULL behind them
             }
             else {
-                ans.emplace_back(curr->val);
+                ans.emplace_back(curr->val); // emplace_back is a faster alternative to push_back
                 if(curr->left) q.push(curr->left);
                 if(curr->right) q.push(curr->right);
             }
