43b0e89887c4b2103243fe6df23c5b2efab9deed
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-29 21:39:50 +0530

    Update Combination Sum-1.cpp

--- Day - 9/Combination Sum-1.cpp
+++ Day - 9/Combination Sum-1.cpp
@@ -30,3 +30,43 @@ void findCombi(int index, int target, vector<int>& arr,
         findCombi(0, target, candidates, ans, ds);
         return ans;
     }
+
+//Sid 
+
+class Solution {
+public:
+    void helper(vector<int> candidates, int target, int i, int sum, set<vector<int>> &res, vector<int> r)
+    {
+        if(i == candidates.size())
+            return;
+        if(sum == target)
+        {
+            res.insert(r);
+            return;
+        }
+        if(sum > target)
+            return;
+        helper(candidates, target, i+1, sum, res, r);
+        r.push_back(candidates[i]);
+        helper(candidates, target, i, sum+candidates[i], res, r);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        set<vector<int>> res;
+        vector<int> r;
+        sort(candidates.begin(), candidates.end());
+        vector<int> candidate;
+        for(int i = 0; i < candidates.size(); i++)
+        {
+            while(i < candidates.size()-1 && candidates[i] == candidates[i+1])
+                i++;
+            candidate.push_back(candidates[i]);
+        }
+        helper(candidate, target, 0, 0, res, r);
+        vector<vector<int>> result;
+        for(auto it = res.begin(); it != res.end(); it++)
+        {
+            result.push_back(*it);
+        }
+        return result;
+    }
+};
