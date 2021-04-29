d74a04f84c1e2550733cf19c9c1fffbb5b8a22d3
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-29 21:41:06 +0530

    Update Combination Sum-2.cpp

--- Day - 9/Combination Sum-2.cpp
+++ Day - 9/Combination Sum-2.cpp
@@ -31,3 +31,32 @@ void findCombi(int ind, int target, vector<int> &candidates,
         findCombi(0, target, candidates, ans, ds);
         return ans;
     }
+
+//Sid 
+
+class Solution {
+public:
+    void helper(vector<int> a, int target, vector<vector<int>> &res, vector<int> r, int sum, int i)
+    {
+        if(sum == target)
+            res.push_back(r);
+        if(sum > target)
+            return;
+        for(int j = i; j < a.size(); j++)
+        {
+            if(j != i && a[j] == a[j-1])
+                continue;
+            r.push_back(a[j]);
+            helper(a, target, res, r, sum+a[j], j+1);
+            r.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target)
+    {
+        vector<vector<int>> res;
+        vector<int> r;
+        sort(candidates.begin(), candidates.end());
+        helper(candidates, target, res, r, 0, 0);
+        return res;
+    }
+};
