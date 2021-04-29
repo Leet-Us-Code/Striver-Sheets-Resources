eeb4b9e66b8368d17014db355341847e9ba60f99
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-04-29 21:41:48 +0530

    Update Palindrome Partitioning.cpp

--- Day - 9/Palindrome Partitioning.cpp
+++ Day - 9/Palindrome Partitioning.cpp
@@ -35,3 +35,41 @@ public:
         return res;
     }
 };
+
+//Sid 
+class Solution {
+public:
+    bool isPalindrome(string s)
+    {
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] != s[s.length() - i - 1])
+                return false;
+        }
+        return true;
+    }
+    void partition(vector<vector<string>> &res, vector<string> &x, string s)
+    {
+        if(s.length() == 0)
+        {
+            res.push_back(x);
+        }
+        for(int i = 0; i < s.length(); i++)
+        {
+            //take the substring from 0 to i 
+            string beg = s.substr(0, i+1);
+            if(isPalindrome(beg))
+            {
+                x.push_back(beg);
+                partition(res, x, s.substr(i+1));
+                x.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> x;
+        partition(res, x, s);
+        return res;
+    }
+};
