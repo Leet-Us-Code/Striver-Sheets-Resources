1c9494905f70b3e44ab7fa52b19784aaa7fe325e
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-02 21:40:13 +0530

    Update Reverse words in String.cpp

--- Day - 15/Reverse words in String.cpp
+++ Day - 15/Reverse words in String.cpp
@@ -13,3 +13,62 @@ string reverseWords(string s) {
         }
         return ans; 
     }
+
+//Sid 
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i, j;
+        i = 0, j = 0;
+        while(i < s.length() && j < s.length())
+        {
+            if(s[i] == ' ')
+            {
+                while(i < s.length()-1 && s[i] == s[i+1]) 
+                    i++;
+                s[j] = s[i];
+                j++;
+                i++;
+            }
+            else
+            {
+                s[j] = s[i];
+                j++;
+                i++;
+            }
+        }
+        s = s.substr(0,j);
+        if(s[0] == ' ')
+        {
+            s = s.substr(1, s.length()-1);
+        }
+        if(s[s.length()-1] == ' ')
+        {
+            s = s.substr(0, s.length() - 1);
+        }
+        reverse(s.begin(), s.end());
+        i = 0;
+        j = 0;
+        for(i = 0; i < s.length(); i++)
+        {
+            while(i < s.length() && s[i] != ' ')
+            {
+                i++;
+            }
+            int r = i - 1;
+            i++;
+            int l = j;
+            while(l <= r)
+            {
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
+            j = i;
+        }
+        return s;
+    }
+};
+
+
