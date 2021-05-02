44072615eaebddf73f9d21e4537650584a599c76
HariHaran <haran465@gmail.com>
2021-05-02 19:06:43 +0530

    Update Rotten Oranges.cpp

--- Day - 14/Rotten Oranges.cpp
+++ Day - 14/Rotten Oranges.cpp
@@ -1,3 +1,47 @@
+// Hari
+
+int orangesRotting(vector<vector<int>>& grid) {
+        // solved using BFS
+        queue<pair<int, int>> rotten;
+        
+        int freshCnt = 0;
+        for(int i = 0; i<grid.size(); i++){
+            for(int j = 0; j<grid[0].size(); j++){
+                if(grid[i][j] == 2) rotten.push(make_pair(i,j)); // add coordinate to queue
+                if(grid[i][j] == 1) freshCnt++; 
+            }
+        }
+        
+        vector<vector<int>> dxy = {{1,0}, {-1,0}, {0,1}, {0,-1}}; // down up right left
+        int time = 0;
+        while(!rotten.empty() && freshCnt > 0){
+            time++;
+            int sz = rotten.size();
+            while(sz-- > 0){
+                auto pick = rotten.front(); rotten.pop();
+                for(auto it: dxy){
+                    int xC = pick.first + it[0]; // x coordinate 
+                    int yC = pick.second + it[1]; // y coordinate
+                    // check bounds or if no fresh orange in x and yCoords
+                    if(xC < 0 || xC >= grid.size() || yC < 0 || yC >= grid[0].size() ||
+                      grid[xC][yC] == 0 || grid[xC][yC] == 2) continue;
+                    
+                    // so we have a fresh orange here. It becomes rotten so add to rotten Q and change grid val from 1 to 2
+                    rotten.push(make_pair(xC, yC));
+                    grid[xC][yC] = 2;
+                    freshCnt--;
+                }
+            }
+        }
+        
+        if(freshCnt == 0) return time;
+        else return -1;
+    }
+
+
+
+
+// Sid
 class Solution {
 public:
     struct Node{
