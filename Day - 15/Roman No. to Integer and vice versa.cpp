e47b542b09bc9e5ae1f3a4d59abad6372c53b30c
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-02 23:03:44 +0530

    Update Roman No. to Integer and vice versa.cpp

--- Day - 15/Roman No. to Integer and vice versa.cpp
+++ Day - 15/Roman No. to Integer and vice versa.cpp
@@ -52,3 +52,98 @@ string intToRoman(int A) {
 
         return res;
         }
+
+
+//Sid 
+//Integer to Roman 
+class Solution {
+public:
+    string intToRoman(int num) {
+        string s;
+        vector<pair<string, int>> mp;
+        mp.push_back({"I", 1});
+        mp.push_back({"IV", 4});
+        mp.push_back({"V", 5});
+        mp.push_back({"IX",9});
+        mp.push_back({"X", 10});
+        mp.push_back({"XL",40});
+        mp.push_back({"L", 50});
+        mp.push_back({"XC", 90});
+        mp.push_back({"C", 100});
+        mp.push_back({"CD", 400});
+        mp.push_back({"D", 500});
+        mp.push_back({"CM",900});
+        mp.push_back({"M", 1000});
+        while(num > 0)
+        {
+            int x;
+            for(int i = 0; i < mp.size(); i++)
+            {
+                if(mp[i].second <= num)
+                {
+                    x = i;
+                }
+            }
+            s += mp[x].first;
+            num = num - mp[x].second;
+            
+        }
+        return s;
+    }
+};
+
+//roman to integer
+class Solution {
+public:
+    int romanToInt(string s) {
+        vector<int> x;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == 'I')
+            {
+                x.push_back(1);
+            }
+            else if(s[i] == 'V')
+            {
+                x.push_back(5);
+            }
+            else if(s[i] == 'X')
+            {
+                x.push_back(10);
+            }
+            else if(s[i] == 'L')
+            {
+                x.push_back(50);
+            }
+            else if(s[i] == 'C')
+            {
+                x.push_back(100);
+            }
+            else if(s[i] == 'D')
+            {
+                x.push_back(500);
+            }
+            else if(s[i] == 'M')
+            {
+                x.push_back(1000);
+            }
+        }
+        int num = 0;
+        int n = x.size();
+        int c = n-1;
+        while(c >= 0)
+        {
+            if(c > 0 && x[c] > x[c-1])
+            {
+                num += (x[c] - x[c-1]);
+                c = c - 2;
+            }
+            else 
+            {
+                num += x[c];
+                c--;
+            }
+        }
+        return num;
+    }
+};
