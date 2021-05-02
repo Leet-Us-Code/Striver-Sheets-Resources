8b29e8d2020acf5896bc2c1f53b29a64020a13bf
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-02 08:27:54 +0530

    Update Sliding Window Maximum.cpp

--- Day - 14/Sliding Window Maximum.cpp
+++ Day - 14/Sliding Window Maximum.cpp
@@ -16,3 +16,62 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         }
         return ans;
     }
+
+
+//Sid's solution
+
+  
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        //first find the next greater element 
+        //in a window if  the next greater element of an element is outside the window then tha will be the maximum element in the window. If the next greater element
+        //exists in the window then go to the next greater element and check it again, then mark it the greatest.
+        int nge[100000];
+        stack<int> s;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            if(s.empty())
+            {
+                s.push(i);
+            }
+            else 
+            {
+                if(nums[i] > nums[s.top()])
+                {
+                    while(!s.empty() && nums[i] > nums[s.top()])
+                    {
+                        nge[s.top()] = i;
+                        s.pop();
+                    }
+                    s.push(i);
+                }
+                else
+                {
+                    s.push(i);
+                }
+            }
+        }
+        while(!s.empty())
+        {
+            nge[s.top()] = n;
+            s.pop();
+        }
+        vector<int> res;
+        int j = 0;
+        for(int i = 0; i <= n-k; i++)
+        {
+            if(j < i)
+            {
+                j = i;
+            }
+            while(nge[j] < i+k)
+            {
+                j = nge[j];
+            }
+            res.push_back(nums[j]);
+        }
+        return res;
+    }
+};
