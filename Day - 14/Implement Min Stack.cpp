841116be1c5debc8cbb8c87a5cfc5bcf79ff4dd2
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-02 08:29:05 +0530

    Update Implement Min Stack.cpp

--- Day - 14/Implement Min Stack.cpp
+++ Day - 14/Implement Min Stack.cpp
@@ -29,3 +29,59 @@
     int getMin() {
         return stk.top().second;
     }
+
+//Sid's solution 
+/*returns min element from stack*/
+int _stack :: getMin()
+{
+   //Your code here
+   if(s.empty())
+   return -1;
+   else
+   return minEle;
+}
+
+/*returns poped element from stack*/
+int _stack ::pop()
+{
+   //Your code here
+   if(s.empty())
+   return -1;
+   else
+   {
+       
+       if(s.top() >= minEle)
+       {
+           int x = s.top();
+           s.pop();
+           return x;
+       }
+       else
+       {
+           int x = minEle;
+           minEle = 2*x - s.top();
+           s.pop();
+           return x;
+       }
+   }
+}
+
+/*push element x into the stack*/
+void _stack::push(int x)
+{
+   //Your code here
+   if(s.empty())
+   {
+       minEle = x;
+       s.push(x);
+   }
+   else if(x >= minEle)
+   {
+       s.push(x);
+   }
+   else
+   {
+       s.push(2*x-minEle);
+       minEle = x;
+   }
+}
