0e8af00104ecab7a0154897c6fb5e8e434c1daba
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-18 22:06:54 +0530

    Update Count and Say.cpp

--- Day - 16/Count and Say.cpp
+++ Day - 16/Count and Say.cpp
@@ -26,3 +26,39 @@ string countAndSay(int A) {
     }
     return ans;
 }
+
+//Sid 
+//Recursive solution
+class Solution {
+public:
+    string helper(int cur, string &s, int n)
+    {
+        if(cur > n)
+            return s;
+        else if(cur == 1)
+        {
+            s.push_back('1');
+            return helper(cur+1, s, n);
+        }
+        else 
+        {
+            string s1;
+            for(int i = 0; i < s.length(); i++)
+            {
+                int c = 1;
+                while(i < s.length()-1 && s[i] == s[i+1])
+                {
+                    i++;
+                    c++;
+                }
+                s1.push_back((char)(c+'0'));
+                s1.push_back((char)(s[i]));
+            }
+            return helper(cur+1, s1, n);
+        }
+    }
+    string countAndSay(int n) {
+        string s;
+        return helper(1, s, n);
+    }
+};
