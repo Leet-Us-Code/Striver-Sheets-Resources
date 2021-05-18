beaa4119a7f6d4237154ca72a7b934b4a5f53dce
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-18 19:40:34 +0530

    Update Check for anagrams.cpp

--- Day - 16/Check for anagrams.cpp
+++ Day - 16/Check for anagrams.cpp
@@ -16,3 +16,27 @@ bool isAnagram(string s, string t) {
         }
         return true;
     }
+
+//Sid's solution
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq1[128] = {0};
+        int freq2[128] = {0};
+        for(int i = 0; i < s.length(); i++)
+        {
+            freq1[s[i]]++;
+        }
+        for(int i = 0; i < t.length(); i++)
+        {
+            freq2[t[i]]++;
+        }
+        for(int i = 0; i < 128; i++)
+        {
+            if(freq1[i] != freq2[i])
+                return false;
+        }
+        return true;
+    }
+};
