745639cdb62b6bc1c434f20fcac88f3139c6d63e
HariHaran <haran465@gmail.com>
2021-05-20 15:14:38 +0530

    Update Top View Of Binary Tree.cpp

--- Day - 17/Top View Of Binary Tree.cpp
+++ Day - 17/Top View Of Binary Tree.cpp
@@ -1,3 +1,41 @@
+// Hari
+
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        // first element you get for a particular vertical level is the element u see from top
+        map<int,int> m; // level and corresp element
+        /*
+        -2 -1 0 1 2 are the vertical levels. 
+        If element is in a level and current val for that level is 0, then add this element to
+        that level. Else, that element is not visible at that level. So, continue
+        */
+        queue<pair<Node*, int>> q; // Element and corresp. height
+        vector<int> res;
+        if(!root) return res;
+        q.push({root, 0}); 
+        while(!q.empty()){
+            Node* curr = q.front().first;
+            int currht = q.front().second;
+            q.pop();
+            if(!m[currht]) m[currht] = curr->data; // if val for this currht already exists, continue
+            if(curr->left) q.push({curr->left, currht-1});
+            if(curr->right) q.push({curr->right, currht+1});
+        }
+        // our map m has all the elements visible from top at each vertical level
+        for(auto t: m){
+            res.push_back(t.second);
+        }
+        return res;
+        
+    }
+
+};
+
 // Sid
 
 class Solution
@@ -43,44 +81,6 @@ class Solution
             res.push_back(it->second);
         }
         return res;
-    }
-
-};
-
-// Hari
-
-class Solution
-{
-    public:
-    //Function to return a list of nodes visible from the top view 
-    //from left to right in Binary Tree.
-    vector<int> topView(Node *root)
-    {
-        // first element you get for a particular vertical level is the element u see from top
-        map<int,int> m; // level and corresp element
-        /*
-        -2 -1 0 1 2 are the vertical levels. 
-        If element is in a level and current val for that level is 0, then add this element to
-        that level. Else, that element is not visible at that level. So, continue
-        */
-        queue<pair<Node*, int>> q; // Element and corresp. height
-        vector<int> res;
-        if(!root) return res;
-        q.push({root, 0}); 
-        while(!q.empty()){
-            Node* curr = q.front().first;
-            int currht = q.front().second;
-            q.pop();
-            if(!m[currht]) m[currht] = curr->data; // if val for this currht already exists, continue
-            if(curr->left) q.push({curr->left, currht-1});
-            if(curr->right) q.push({curr->right, currht+1});
-        }
-        // our map m has all the elements visible from top at each vertical level
-        for(auto t: m){
-            res.push_back(t.second);
-        }
-        return res;
-        
     }
 
 };
