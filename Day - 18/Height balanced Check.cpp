5e13a4af29601378e76103a4bd30e40eedfe96bd
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-23 09:42:51 +0530

    Update Height balanced Check.cpp

--- Day - 18/Height balanced Check.cpp
+++ Day - 18/Height balanced Check.cpp
@@ -19,3 +19,25 @@ public:
         return htBalChk;
     }
 };
+
+//Sid 
+bool helper(TreeNode *root, int &h)
+    {
+        if(root == NULL)
+        {
+            h = 0; 
+            return true;
+        }
+        int lh = 0, rh = 0;
+        bool l = helper(root->left, lh);
+        bool r = helper(root->right, rh);
+        h = max(lh, rh) + 1;
+        if(l && r && abs(lh-rh) <= 1)
+            return true;
+        else
+            return false;
+    }
+    bool isBalanced(TreeNode* root) {
+        int height = 0;
+        return helper(root, height);
+    }
