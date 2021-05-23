9a36085ef53a8311446ad601738216596736a5a0
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-23 15:58:07 +0530

    Update Symmetric Tree.cpp

--- Day - 19/Symmetric Tree.cpp
+++ Day - 19/Symmetric Tree.cpp
@@ -18,3 +18,24 @@ public:
         return helper(root, root);
     }
 };
+
+//Sid 
+bool helper(TreeNode *root1, TreeNode *root2)
+    {
+        if(root1 == NULL && root2 == NULL)
+            return true;
+        else if(root1 == NULL || root2 == NULL)
+            return false;
+        bool x = helper(root1->left, root2->right);
+        bool y = helper(root1->right, root2->left);
+        if(root1->val == root2->val && x && y)
+            return true;
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        //OM GAN GANAPATHAYE NAMO NAMAH 
+        //JAI SHRI RAM 
+        //JAI BAJRANGBALI 
+        //AMME NARAYANA, DEVI NARAYANA, LAKSHMI NARAYANA, BHADRE NARAYANA
+        return helper(root->left, root->right);
+    }
