cf43cd041e466cf0b992d51a585bc3cb580e291e
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-25 11:57:53 +0530

    Update Construct BST from given keys.cpp

--- Day - 20/Construct BST from given keys.cpp
+++ Day - 20/Construct BST from given keys.cpp
@@ -32,3 +32,34 @@ public:
         return root;
     }
 };
+
+//Sid
+class Solution {
+public:
+    TreeNode *helper(ListNode *start, ListNode *end)
+    {
+        if(start == end)
+        {
+            return NULL;
+        }
+        if(start->next == end)
+        {
+            TreeNode *root =  new TreeNode(start->val);
+            return root;
+        }
+        ListNode *slow = start, *fast = start;
+        while(fast != end && fast->next != end)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        TreeNode *root = new TreeNode(slow->val);
+        root->left = helper(start, slow);
+        root->right = helper(slow->next, end);
+        return root;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        ListNode *start = head, *end = NULL;
+        return helper(start, end);
+    }
+};
