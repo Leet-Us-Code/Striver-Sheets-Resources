905952a5290a586be4109474c8d580d3f6db8a1f
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-05-25 11:59:15 +0530

    Update Search in BST.cpp

--- Day - 20/Search in BST.cpp
+++ Day - 20/Search in BST.cpp
@@ -14,3 +14,22 @@ public:
         return nullptr;
     }
 };
+
+//Sid 
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL)
+            return root;
+        if(root->val == val)
+            return root;
+        else if(root->val > val)
+        {
+            return searchBST(root->left, val);
+        }
+        else 
+        {
+            return searchBST(root->right, val);
+        }
+    }
+};
