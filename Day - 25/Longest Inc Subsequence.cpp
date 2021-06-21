429da00ffa779bf3951ce28aa2730a985ac8ceb8
Siddharth.S.Chandran <64896383+DiligentCoder-20022001@users.noreply.github.com>
2021-06-21 18:19:48 +0530

    Update Longest Inc Subsequence.cpp

--- Day - 25/Longest Inc Subsequence.cpp
+++ Day - 25/Longest Inc Subsequence.cpp
@@ -22,3 +22,32 @@ int lengthOfLIS(vector<int>& nums) {
         }
         return res;
     }
+
+
+//Sid 
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int lis[100000];
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+            lis[i] = 1;
+        for(int i = 1; i < n ; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[j] < nums[i])
+                {
+                    lis[i] = max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+        int maxEle = INT_MIN;
+        for(int i = 0; i < n; i++)
+        {
+            maxEle = max(maxEle, lis[i]);
+        }
+        return maxEle;
+    }
+};
