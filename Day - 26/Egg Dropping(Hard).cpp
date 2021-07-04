11a14d10557befee714625940b01540be7d5b8c6
HariHaran <haran465@gmail.com>
2021-07-04 13:36:34 +0530

    Update Egg Dropping(Hard).cpp

--- Day - 26/Egg Dropping(Hard).cpp
+++ Day - 26/Egg Dropping(Hard).cpp
@@ -57,4 +57,17 @@ public:
 
 // Method - 2
 
+class Solution {
+public:
+     int superEggDrop(int K, int N) {
+        vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+        int m = 0;
+        while (dp[m][K] < N) {
+            m++;
+            for (int k = 1; k <= K; ++k)
+                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
+        }
+        return m;
+    }
+};
 
